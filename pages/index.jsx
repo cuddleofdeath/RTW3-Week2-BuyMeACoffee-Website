f2b7d4b4ce8b37f1bcf09ae1f8011f88a1176988
cuddleofdeath <cuddleofdeath@gmail.com>
2022-05-27 22:24:23 -0500

    Week2 Complete :D

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,71 +1,71 @@
 import abi from '../utils/BuyMeACoffee.json';
-import { ethers } from "ethers";
-import Head from 'next/head'
-import React, { useEffect, useState } from "react";
-import styles from '../styles/Home.module.css'
+import { ethers } from 'ethers';
+import Head from 'next/head';
+import React, { useEffect, useState } from 'react';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   // Contract Address & ABI
-  const contractAddress = "0xDBa03676a2fBb6711CB652beF5B7416A53c1421D";
+  const contractAddress = '0xc033E29f85D95EE5c8aabC703A6B1Ab50993cBfc';
   const contractABI = abi.abi;
 
   // Component state
-  const [currentAccount, setCurrentAccount] = useState("");
-  const [name, setName] = useState("");
-  const [message, setMessage] = useState("");
+  const [currentAccount, setCurrentAccount] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
   const [memos, setMemos] = useState([]);
 
   const onNameChange = (event) => {
     setName(event.target.value);
-  }
+  };
 
   const onMessageChange = (event) => {
     setMessage(event.target.value);
-  }
+  };
 
   // Wallet connection logic
   const isWalletConnected = async () => {
     try {
       const { ethereum } = window;
 
-      const accounts = await ethereum.request({method: 'eth_accounts'})
-      console.log("accounts: ", accounts);
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      console.log('accounts: ', accounts);
 
       if (accounts.length > 0) {
         const account = accounts[0];
-        console.log("wallet is connected! " + account);
+        console.log('wallet is connected! ' + account);
       } else {
-        console.log("make sure MetaMask is connected");
+        console.log('make sure MetaMask is connected');
       }
     } catch (error) {
-      console.log("error: ", error);
+      console.log('error: ', error);
     }
-  }
+  };
 
   const connectWallet = async () => {
     try {
-      const {ethereum} = window;
+      const { ethereum } = window;
 
       if (!ethereum) {
-        console.log("please install MetaMask");
+        console.log('please install MetaMask');
       }
 
       const accounts = await ethereum.request({
-        method: 'eth_requestAccounts'
+        method: 'eth_requestAccounts',
       });
 
       setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const buyCoffee = async () => {
     try {
-      const {ethereum} = window;
+      const { ethereum } = window;
 
       if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum, "any");
+        const provider = new ethers.providers.Web3Provider(ethereum, 'any');
         const signer = provider.getSigner();
         const buyMeACoffee = new ethers.Contract(
           contractAddress,
@@ -73,22 +73,22 @@ export default function Home() {
           signer
         );
 
-        console.log("buying coffee..")
+        console.log('buying coffee..');
         const coffeeTxn = await buyMeACoffee.buyCoffee(
-          name ? name : "anon",
-          message ? message : "Enjoy your coffee!",
-          {value: ethers.utils.parseEther("0.001")}
+          name ? name : 'anon',
+          message ? message : 'Enjoy your coffee!',
+          { value: ethers.utils.parseEther('0.001') }
         );
 
         await coffeeTxn.wait();
 
-        console.log("mined ", coffeeTxn.hash);
+        console.log('mined ', coffeeTxn.hash);
 
-        console.log("coffee purchased!");
+        console.log('coffee purchased!');
 
         // Clear the form fields.
-        setName("");
-        setMessage("");
+        setName('');
+        setMessage('');
       }
     } catch (error) {
       console.log(error);
@@ -107,20 +107,19 @@ export default function Home() {
           contractABI,
           signer
         );
-        
-        console.log("fetching memos from the blockchain..");
+
+        console.log('fetching memos from the blockchain..');
         const memos = await buyMeACoffee.getMemos();
-        console.log("fetched!");
+        console.log('fetched!');
         setMemos(memos);
       } else {
-        console.log("Metamask is not connected");
+        console.log('Metamask is not connected');
       }
-      
     } catch (error) {
       console.log(error);
     }
   };
-  
+
   useEffect(() => {
     let buyMeACoffee;
     isWalletConnected();
@@ -129,90 +128,76 @@ export default function Home() {
     // Create an event handler function for when someone sends
     // us a new memo.
     const onNewMemo = (from, timestamp, name, message) => {
-      console.log("Memo received: ", from, timestamp, name, message);
+      console.log('Memo received: ', from, timestamp, name, message);
       setMemos((prevState) => [
         ...prevState,
         {
           address: from,
           timestamp: new Date(timestamp * 1000),
           message,
-          name
-        }
+          name,
+        },
       ]);
     };
 
-    const {ethereum} = window;
+    const { ethereum } = window;
 
     // Listen for new memo events.
     if (ethereum) {
-      const provider = new ethers.providers.Web3Provider(ethereum, "any");
+      const provider = new ethers.providers.Web3Provider(ethereum, 'any');
       const signer = provider.getSigner();
-      buyMeACoffee = new ethers.Contract(
-        contractAddress,
-        contractABI,
-        signer
-      );
+      buyMeACoffee = new ethers.Contract(contractAddress, contractABI, signer);
 
-      buyMeACoffee.on("NewMemo", onNewMemo);
+      buyMeACoffee.on('NewMemo', onNewMemo);
     }
 
     return () => {
       if (buyMeACoffee) {
-        buyMeACoffee.off("NewMemo", onNewMemo);
+        buyMeACoffee.off('NewMemo', onNewMemo);
       }
-    }
+    };
   }, []);
-  
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Buy Albert a Coffee!</title>
-        <meta name="description" content="Tipping site" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Buy CuddleofDeath a Coffee!</title>
+        <meta name='description' content='Tipping site' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Buy Albert a Coffee!
-        </h1>
-        
+        <h1 className={styles.title}>Buy CuddleofDeath a Coffee!</h1>
+
         {currentAccount ? (
           <div>
             <form>
-              <div class="formgroup">
-                <label>
-                  Name
-                </label>
-                <br/>
-                
+              <div class='formgroup'>
+                <label>Name</label>
+                <br />
+
                 <input
-                  id="name"
-                  type="text"
-                  placeholder="anon"
+                  id='name'
+                  type='text'
+                  placeholder='anon'
                   onChange={onNameChange}
-                  />
+                />
               </div>
-              <br/>
-              <div class="formgroup">
-                <label>
-                  Send Albert a message
-                </label>
-                <br/>
+              <br />
+              <div class='formgroup'>
+                <label>Send CuddleofDeath a message</label>
+                <br />
 
                 <textarea
                   rows={3}
-                  placeholder="Enjoy your coffee!"
-                  id="message"
+                  placeholder='Enjoy your coffee!'
+                  id='message'
                   onChange={onMessageChange}
                   required
-                >
-                </textarea>
+                ></textarea>
               </div>
               <div>
-                <button
-                  type="button"
-                  onClick={buyCoffee}
-                >
+                <button type='button' onClick={buyCoffee}>
                   Send 1 Coffee for 0.001ETH
                 </button>
               </div>
@@ -223,26 +208,37 @@ export default function Home() {
         )}
       </main>
 
-      {currentAccount && (<h1>Memos received</h1>)}
+      {currentAccount && <h1>Memos received</h1>}
 
-      {currentAccount && (memos.map((memo, idx) => {
-        return (
-          <div key={idx} style={{border:"2px solid", "border-radius":"5px", padding: "5px", margin: "5px"}}>
-            <p style={{"font-weight":"bold"}}>"{memo.message}"</p>
-            <p>From: {memo.name} at {memo.timestamp.toString()}</p>
-          </div>
-        )
-      }))}
+      {currentAccount &&
+        memos.map((memo, idx) => {
+          return (
+            <div
+              key={idx}
+              style={{
+                border: '2px solid',
+                'border-radius': '5px',
+                padding: '5px',
+                margin: '5px',
+              }}
+            >
+              <p style={{ 'font-weight': 'bold' }}>"{memo.message}"</p>
+              <p>
+                From: {memo.name} at {memo.timestamp.toString()}
+              </p>
+            </div>
+          );
+        })}
 
       <footer className={styles.footer}>
         <a
-          href="https://alchemy.com/?a=roadtoweb3weektwo"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://alchemy.com/?a=roadtoweb3weektwo'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Created by @thatguyintech for Alchemy's Road to Web3 lesson two!
         </a>
       </footer>
     </div>
-  )
+  );
 }
